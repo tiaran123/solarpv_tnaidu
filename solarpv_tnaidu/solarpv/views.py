3951d4a766f8839fc3a9b894dd7aa354d9c7d1f6
tiaran123 <91352441+tiaran123@users.noreply.github.com>
2021-10-07 18:57:12 -0700

    almost done

---
+++ solarpv_tnaidu/solarpv/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'solarpv/project1.html', dict())
+
+
+def form(request):
+    return render(request, 'solarpv/form.html', dict())
+
+def login(request):
+    return render(request, 'solarpv/login.html', dict())
+
+def webPortal(request):
+    return render(request, 'solarpv/webPortal.html', dict())
